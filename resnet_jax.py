786a53424a67fb2f5f22a0d864a895bc729f9940
baofff <2978777543@qq.com>
2022-08-02 23:43:38 +0800

    Update resnet_jax.py

--- resnet_jax.py
+++ resnet_jax.py
@@ -1,157 +1,75 @@
-from typing import Any, Callable, List, Optional
-
-import torch
-import torch.nn as nn
-from torch import Tensor
+from typing import Any, Callable, List
+from flax import linen as nn
+import einops
 
 
-def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
-    """3x3 convolution with padding"""
-    return nn.Conv2d(
-        in_planes,
-        out_planes,
-        kernel_size=3,
-        stride=stride,
-        padding=dilation,
-        groups=groups,
-        bias=False,
-        dilation=dilation,
-    )
+def conv3x3(out_planes: int, stride: int = 1, kernel_init=nn.initializers.kaiming_normal):
+    return nn.Conv(out_planes, kernel_size=(3, 3), strides=(stride, stride), use_bias=False, kernel_init=kernel_init)
 
 
-def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+def conv1x1(out_planes: int, stride: int = 1, kernel_init=nn.initializers.kaiming_normal):
+    return nn.Conv(out_planes, kernel_size=(1, 1), strides=(stride, stride), use_bias=False, kernel_init=kernel_init)
 
 
 class BasicBlock(nn.Module):
-    def __init__(
-        self,
-        inplanes: int,
-        planes: int,
-        stride: int = 1,
-        downsample: Optional[nn.Module] = None,
-        norm_layer: Optional[Callable[..., nn.Module]] = None,
-    ) -> None:
-        super().__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
+    planes: int
+    stride: int = 1
+    norm_layer: Callable = None
 
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x: Tensor) -> Tensor:
+    @nn.compact
+    def __call__(self, x):
+        in_planes = x.shape[-1]
         identity = x
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
+        out = conv3x3(self.planes, self.stride)(x)
+        out = self.norm_layer(self.planes)(out)
+        out = nn.relu(out)
 
-        out = self.conv2(out)
-        out = self.bn2(out)
+        out = conv3x3(self.planes)(out)
+        out = self.norm_layer(self.planes)(out)
 
-        if self.downsample is not None:
-            identity = self.downsample(x)
+        if self.stride != 1 or self.planes != in_planes:
+            identity = conv1x1(self.planes, self.stride)
+            identity = self.norm_layer(self.planes)(identity)
 
         out += identity
-        out = self.relu(out)
+        out = nn.relu(out)
 
         return out
 
 
+class BasicLayer(nn.Module):
+    planes: int
+    blocks: int
+    stride: int = 1
+    norm_layer: Callable = None
+
+    @nn.compact
+    def __call__(self, x):
+        for i in range(self.blocks):
+            x = BasicBlock(self.planes, self.stride if i == 0 else 1, self.norm_layer)(x)
+        return x
+
+
 class ResNet(nn.Module):
-    def __init__(
-        self,
-        layers: List[int],
-        num_classes: int = 1000,
-        zero_init_residual: bool = False,
-        norm_layer: Optional[Callable[..., nn.Module]] = None,
-    ) -> None:
-        super().__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self._norm_layer = norm_layer
+    layers: List[int]
+    num_classes: int = 10
+    zero_init_residual: bool = False
+    norm_layer: Callable = nn.BatchNorm
 
-        self.inplanes = 64
-
-        # self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(64, layers[0])
-        self.layer2 = self._make_layer(128, layers[1], stride=2)
-        self.layer3 = self._make_layer(256, layers[2], stride=2)
-        self.layer4 = self._make_layer(512, layers[3], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                assert isinstance(m, BasicBlock)
-                nn.init.constant_(m.bn2.weight, 0)
-
-    def _make_layer(
-        self,
-        planes: int,
-        blocks: int,
-        stride: int = 1,
-    ) -> nn.Sequential:
-        norm_layer = self._norm_layer
-        downsample = None
-
-        if stride != 1 or self.inplanes != planes:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes, stride),
-                norm_layer(planes),
-            )
-
-        layers = [BasicBlock(
-            self.inplanes, planes, stride, downsample, norm_layer
-        )]
-        self.inplanes = planes
-        for _ in range(1, blocks):
-            layers.append(
-                BasicBlock(
-                    self.inplanes,
-                    planes,
-                    norm_layer=norm_layer,
-                )
-            )
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        # x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.fc(x)
-
+    @nn.compact
+    def __call__(self, x):
+        inplanes = 64
+        x = conv3x3(inplanes)(x)
+        x = self.norm_layer()(x)
+        x = nn.relu(x)
+        x = BasicLayer(64, self.layers[0])(x)
+        x = BasicLayer(128, self.layers[1], 2, self.norm_layer)(x)
+        x = BasicLayer(256, self.layers[2], 2, self.norm_layer)(x)
+        x = BasicLayer(512, self.layers[3], 2, self.norm_layer)(x)
+        x = nn.avg_pool(x, x.shape[1:3])
+        x = einops.rearrange(x, 'B () () C -> B C')
+        x = nn.Dense(self.num_classes)(x)
         return x
 
 
