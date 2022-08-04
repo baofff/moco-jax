0adbf2e08e8403d66fa54506a025bf5fae6b0089
Fan Bao <31411327+baofff@users.noreply.github.com>
2022-08-04 17:33:23 +0800

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -4,6 +4,64 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 from moco_cifar10_demo import ModelBase
 import torch
+import torch.nn as nn
+
+
+class SplitBatchNorm(nn.BatchNorm2d):
+    def __init__(self, num_features, num_splits, **kw):
+        super().__init__(num_features, **kw)
+        self.num_splits = num_splits
+
+    def forward(self, input):
+        N, C, H, W = input.shape
+        if self.training and self.track_running_stats:
+            running_mean_split = self.running_mean.repeat(self.num_splits)
+            running_var_split = self.running_var.repeat(self.num_splits)
+            outcome = nn.functional.batch_norm(
+                input.view(-1, C * self.num_splits, H, W), running_mean_split, running_var_split,
+                self.weight.repeat(self.num_splits), self.bias.repeat(self.num_splits),
+                True, self.momentum, self.eps).view(N, C, H, W)
+            self.running_mean.data.copy_(running_mean_split.view(self.num_splits, C).mean(dim=0))
+            self.running_var.data.copy_(running_var_split.view(self.num_splits, C).mean(dim=0))
+            return outcome
+        else:
+            return nn.functional.batch_norm(
+                input, self.running_mean, self.running_var,
+                self.weight, self.bias, False, self.momentum, self.eps)
+
+
+class ModelBase(nn.Module):
+    """
+    Common CIFAR ResNet recipe.
+    Comparing with ImageNet ResNet recipe, it:
+    (i) replaces conv1 with kernel=3, str=1
+    (ii) removes pool1
+    """
+
+    def __init__(self, feature_dim=128, arch=None, bn_splits=16):
+        super(ModelBase, self).__init__()
+
+        # use split batchnorm
+        norm_layer = partial(SplitBatchNorm, num_splits=bn_splits) if bn_splits > 1 else nn.BatchNorm2d
+        resnet_arch = getattr(resnet, arch)
+        net = resnet_arch(num_classes=feature_dim, norm_layer=norm_layer)
+
+        self.net = []
+        for name, module in net.named_children():
+            if name == 'conv1':
+                module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+            if isinstance(module, nn.MaxPool2d):
+                continue
+            if isinstance(module, nn.Linear):
+                self.net.append(nn.Flatten(1))
+            self.net.append(module)
+
+        self.net = nn.Sequential(*self.net)
+
+    def forward(self, x):
+        x = self.net(x)
+        # note: not normalized here
+        return x
 
 
 def kmeans(data, K):
